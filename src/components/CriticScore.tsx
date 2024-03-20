74672c2ec8a1ee1087e872ce9aae7e9d90491da5
suelahasanaj <suelahasanaj6@gmail.com>
2024-03-20 15:53:34 +0100

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge colorScheme={color} paddingX={2} fontSize="14px" borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
