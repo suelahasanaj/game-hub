5f834fd8353a8c1a335af8e749d7cd5d8ef30742
suelahasanaj <suelahasanaj6@gmail.com>
2024-04-09 15:56:45 +0200

    Improve the layout and fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,20 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  //make the nav bar to the full screen width
+
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
+      <Box w="full">
+        <SearchInput />
+      </Box>
       <ColorModeSwitch />
     </HStack>
   );
